0a035a427078a3649d940e76d1bde097af2f43fa
Tyrone <38696@ma-web.nl>
2024-11-29 10:10:56 +0100

    first commit

--- functions/functions/Program.cs
+++ functions/functions/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace functions
+﻿using System.ComponentModel.Design;
+using System.Net.Security;
+
+namespace functions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program.Run();
+      
+        }
+        //Program
+
+        internal void Run()
+        {
+
+            Console.WriteLine("Hello World");
         }
     }
 }
