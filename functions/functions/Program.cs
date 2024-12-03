d1d848b070706b9da5f584e495caa9e4ae14a478
Tyrone <38696@ma-web.nl>
2024-12-03 10:53:31 +0100

    03

--- functions/functions/Program.cs
+++ functions/functions/Program.cs
@@ -18,5 +18,16 @@ namespace functions
 
             Console.WriteLine("Hello World");
         }
+
+        internal void Vraag1()
+        {
+
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+
+
+            Console.WriteLine(antwoord);
+        }  
+
     }
 }
