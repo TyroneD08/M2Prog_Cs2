c70c63bd86635d78a060f151ef18e74a8e58f139
Tyrone <38696@ma-web.nl>
2024-12-06 10:12:44 +0100

    update

--- functions/functions/Program.cs
+++ functions/functions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Design;
 using System.Net.Security;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace functions
 {
@@ -9,7 +10,7 @@ namespace functions
         {
             Program program = new Program();
             program.Run();
-      
+
         }
         //Program
 
@@ -17,17 +18,85 @@ namespace functions
         {
 
             Console.WriteLine("Hello World");
+            Vraag7();
         }
 
-        internal void Vraag1()
+        internal void Vraag7()
         {
 
             Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
             string antwoord = Console.ReadLine();
 
+            Console.WriteLine(antwoord);
+        }
+
+
+        internal void Vraag1()
+        {
+
+
+
+            Console.WriteLine(" If you can still remember, what are your funniest childhood memories?");
+            string antwoord = Console.ReadLine();
 
             Console.WriteLine(antwoord);
-        }  
+        }
+
+        internal void Vraag2()
+        {
+
+
+
+            Console.WriteLine(" What would you do if you won a million dollars?");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+        }
+
+
+        internal void Vraag3()
+        {
+
+
+
+            Console.WriteLine("Why do round pizzas come in square boxes?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag4()
+        {
+
+
+
+            Console.WriteLine("Which species would be the rudest if all animals could talk?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+
+        internal void Vraag5()
+        {
+
+
+
+            Console.WriteLine("What is the worst game you played but that you liked anyway?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag6()
+        {
+
+
+
+            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
 
     }
 }
